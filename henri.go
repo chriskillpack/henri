b10fe93e9b5d927d8ed7f8f43752e2eb74014758
Chris Killpack <chris@chriskillpack.com>
2025-02-07 12:19:41 -0800

    How did I miss this file?

---
+++ henri.go
@@ -0,0 +1,52 @@
+package henri
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/chriskillpack/henri/describer"
+	"github.com/chriskillpack/henri/internal/llama"
+	"github.com/chriskillpack/henri/internal/ollama"
+)
+
+type InitOptions struct {
+	LlamaServer string
+	LlamaSeed   int
+
+	OllamaServer string
+
+	httpClient *http.Client // if nil uses http.DefaultClient
+}
+
+type Henri struct {
+	serverType int // 0 = llama, 1 = ollama
+
+	describer.Describer
+}
+
+func Init(hio InitOptions) (*Henri, error) {
+	h := &Henri{}
+
+	if hio.LlamaServer == "" && hio.OllamaServer == "" {
+		return nil, fmt.Errorf("needs llama or ollama server")
+	}
+	if hio.LlamaServer != "" && hio.OllamaServer != "" {
+		return nil, fmt.Errorf("cannot use both llama and ollama together")
+	}
+
+	httpClient := hio.httpClient
+	if httpClient == nil {
+		httpClient = http.DefaultClient
+	}
+
+	if hio.LlamaServer != "" {
+		h.Describer = llama.Init(hio.LlamaServer, hio.LlamaSeed)
+		h.serverType = 0
+	}
+	if hio.OllamaServer != "" {
+		h.Describer = ollama.Init(hio.OllamaServer)
+		h.serverType = 1
+	}
+
+	return h, nil
+}

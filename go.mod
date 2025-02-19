083cd5c16366716573fa41fcdd6842f6c80c793b
Chris Killpack <chris@chriskillpack.com>
2025-02-19 12:20:43 -0800

    Use the official OpenAI Go Client library
    No need to roll things ourselves. Also the OpenAI client does retries
    with backoff.

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.23
 toolchain go1.23.6
 
 require (
+	github.com/openai/openai-go v0.1.0-alpha.59
 	github.com/tailscale/squibble v0.0.0-20250108170732-a4ca58afa694
 	modernc.org/sqlite v1.34.5
 )
@@ -17,6 +18,10 @@ require (
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/ncruces/go-strftime v0.1.9 // indirect
 	github.com/remyoudompheng/bigfft v0.0.0-20230129092748-24d4a6f8daec // indirect
+	github.com/tidwall/gjson v1.14.4 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.1 // indirect
+	github.com/tidwall/sjson v1.2.5 // indirect
 	golang.org/x/exp v0.0.0-20250128182459-e0ece0dbea4c // indirect
 	golang.org/x/sys v0.30.0 // indirect
 	modernc.org/libc v1.61.11 // indirect
--- internal/openai/openai.go
+++ internal/openai/openai.go
@@ -1,29 +1,26 @@
 package openai
 
 import (
-	"bytes"
 	"context"
-	"encoding/json"
 	"fmt"
-	"io"
 	"net/http"
-	"os"
 	"time"
 
 	"github.com/chriskillpack/henri/describer"
+
+	oagc "github.com/openai/openai-go"
+	"github.com/openai/openai-go/option"
 )
 
 const model = "text-embedding-3-small"
 
 type openai struct {
-	client *http.Client
-	model  string
+	oac   *oagc.Client
+	model string
 }
 
 var (
 	_ describer.Describer = &openai{}
-
-	openai_key string // This will be populated in init()
 
 	rl *rateLimiter // For requests to the OpenAI API
 
@@ -34,15 +31,6 @@ var (
 	}
 )
 
-func init() {
-	if openai_key == "" {
-		openai_key = os.Getenv("OPENAI_API_KEY")
-		if openai_key == "" {
-			panic("OPENAI_API_KEY is not set")
-		}
-	}
-}
-
 func Init(httpClient *http.Client) *openai {
 	if _, ok := modelDimensions[model]; !ok {
 		panic("Unrecognized model")
@@ -50,7 +38,12 @@ func Init(httpClient *http.Client) *openai {
 
 	rl = newRateLimiter(20, time.Minute)
 
-	return &openai{client: httpClient, model: model}
+	return &openai{
+		oac: oagc.NewClient(
+			option.WithHTTPClient(httpClient),
+		),
+		model: model,
+	}
 }
 
 func (o *openai) Name() string { return "openai" }
@@ -67,80 +60,29 @@ func (o *openai) IsHealthy() bool {
 }
 
 func (o *openai) Embeddings(ctx context.Context, description string) ([]float32, error) {
-	reqData := struct {
-		Input      string `json:"input"`
-		Model      string `json:"model"`
-		Dimensions int    `json:"dimensions"`
-	}{
-		Input:      description,
-		Model:      o.model,
-		Dimensions: modelDimensions[o.model],
-	}
-
-	type embedding struct {
-		Object string    `json:"object"`
-		Vector []float32 `json:"embedding"`
-	}
-
-	type usage struct {
-		PromptTokens int `json:"prompt_tokens"`
-		TotalTokens  int `json:"total_tokens"`
-	}
-
-	respData := struct {
-		Object    string      `json:"object"`
-		Embedding []embedding `json:"data"`
-		Model     string      `json:"model"`
-		Usage     usage       `json:"usage"`
-	}{}
-
 	// Rate limit use of the OpenAI API
 	if err := rl.Acquire(ctx); err != nil {
 		return nil, err
 	}
 
-	if err := o.sendRequest(ctx, "https://api.openai.com/v1/embeddings", reqData, &respData); err != nil {
+	enp := oagc.EmbeddingNewParams{
+		Input:      oagc.F(oagc.EmbeddingNewParamsInputUnion(oagc.EmbeddingNewParamsInputArrayOfStrings{description})),
+		Model:      oagc.F(oagc.EmbeddingModel(o.model)),
+		Dimensions: oagc.Int(int64(modelDimensions[o.model])),
+	}
+	resp, err := o.oac.Embeddings.New(ctx, enp)
+	if err != nil {
 		return nil, err
 	}
-	if respData.Embedding[0].Object != "embedding" {
-		return nil, fmt.Errorf("unexpected object type %q", respData.Embedding[0].Object)
+	if resp.Data[0].Object != oagc.EmbeddingObjectEmbedding {
+		return nil, fmt.Errorf("unexpected object type %q", resp.Data[0].Object)
 	}
 
-	return respData.Embedding[0].Vector, nil
-}
-
-func (o *openai) sendRequest(ctx context.Context, path string, reqData, respData any) error {
-	data, err := json.Marshal(reqData)
-	if err != nil {
-		return err
-	}
-	reqBody := bytes.NewReader(data)
-
-	req, err := http.NewRequestWithContext(ctx, http.MethodPost, path, reqBody)
-	if err != nil {
-		return err
-	}
-	req.Header.Set("Content-Type", "application/json")
-	req.Header.Set("Authorization", "Bearer "+openai_key)
-
-	resp, err := o.client.Do(req)
-	if err != nil {
-		return err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode < http.StatusOK || resp.StatusCode >= http.StatusBadRequest {
-		return fmt.Errorf("bad response %d", resp.StatusCode)
+	// Convert the float64 embedding vector to float32
+	embs := make([]float32, len(resp.Data[0].Embedding))
+	for i, em := range resp.Data[0].Embedding {
+		embs[i] = float32(em)
 	}
 
-	respBody, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return err
-	}
-
-	if err := json.Unmarshal(respBody, respData); err != nil {
-		return err
-	}
-
-	return nil
+	return embs, nil
 }
